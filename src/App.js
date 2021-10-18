0f96026197e262ac19232085d4c816e73b846169
Your Name <you@example.com>
2021-10-18 07:06:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Banner from './Pages/Home/Banner/Banner';
+import Blogs from './Pages/Home/Blogs/Blogs';
+import Contact from './Pages/Home/Contact/Contact';
+import Home from './Pages/Home/Home/Home';
+import Service from './Pages/Home/Service/Service';
+import Services from './Pages/Home/Services/Services';
+import Testimonials from './Pages/Home/Testimonials/Testimonials';
+import Login from './Pages/Login/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/home">
+          <Home></Home>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route exact path="/banner">
+          <Banner></Banner>
+        </Route>
+        <Route exact path="/services">
+          <Services></Services>
+        </Route>
+        <Route exact path="/service/:serviceId">
+          <Service></Service>
+        </Route>
+        <Route exact path="/testimonials">
+          <Testimonials></Testimonials>
+        </Route>
+        <Route exact path="/blogs">
+          <Blogs></Blogs>
+        </Route>
+        <Route exact path="/contact">
+          <Contact></Contact>
+        </Route>
+        <Route exact path="/login">
+          <Login></Login>
+        </Route>
+        <Route exact path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
