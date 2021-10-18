16da7e35d7c069d1649cde80d53497222071016c
Your Name <you@example.com>
2021-10-18 09:13:06 +0600

    banner done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
-        </div>
+        <Banner></Banner>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,43 +6,43 @@ import { HashLink } from 'react-router-hash-link';
 const Header = () => {
     return (
         <div>
-            <div className="bg-primary text-white p-2">
-                <h6><i class="fas fa-phone-volume ms-5 me-2"></i>Emergency Contact: 9845037</h6>
-
+            <div className="bg-primary text-white d-flex justify-content-end pt-3">
+               <h6 className="me-3"><i className="fas fa-phone-volume"></i> 9845037</h6>
+               <h6 className="me-3"><i className="far fa-clock me-1"></i>Sun-Sat 8am to 10 pm</h6>
             </div>
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-  <Link class="navbar-brand me-5" to="/home">
-      <img src={headerLogo} alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+  <Link className="navbar-brand me-5" to="/home">
+      <img src={headerLogo} alt="" width="30" height="24" className="d-inline-block align-text-top"/>
     </Link>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" as={HashLink} to="/home#home">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" as={HashLink} to="/home#home">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" as={HashLink} to="/home#services">Services</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" as={HashLink} to="/home#services">Services</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" as={HashLink} to="/departments#departments">Departments</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" as={HashLink} to="/departments#departments">Departments</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" as={HashLink} to="/appointment#appointment">Appointment</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" as={HashLink} to="/appointment#appointment">Appointment</Link>
         </li>
       </ul>
       
     </div>
     <div className="d-flex">
-    <div class="d-flex">
-      <Link class="nav-link active" aria-current="page" to="/login">Login</Link>
-      <Link class="nav-link active" aria-current="page" to="/login">Logout</Link>
+    <div className="d-flex">
+      <Link className="nav-link active" aria-current="page" to="/login">Login</Link>
+      <Link className="nav-link active" aria-current="page" to="/login">Logout</Link>
       <h4 className="me-2 mt-2">Raisa</h4>
-      <img src={headerLogo} alt="" width="30" height="24" class="d-inline-block align-text-top mt-2"/>
+      <img src={headerLogo} alt="" width="30" height="24" className="d-inline-block align-text-top mt-2"/>
       </div>
   </div>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
     </div>
 </nav>
